5659c5259b3e98ef74b6db8ca0a3dde84b33ccab
oussamaef06 <oussamaelfarihi@gmail.com>
2024-01-08 19:21:56 +0100

    updating wiki form

--- views/wiki_form_view.php
+++ views/wiki_form_view.php
@@ -1,38 +1,33 @@
 <section class="min-vh-100 d-flex justify-content-center align-items-center bg-dark">
-  <div class="container">
-    <div class="row justify-content-center">
-      <div class="col-md-8">
-        <form class="mb-5">
-          <h2 class="text-white mb-4">Post Content</h2>
-          <div class="form-group">
-        <input class="form-control py-4" type="text" placeholder="Post heading" />
-        <div class="invalid-feedback">Heading required</div>
-        <div class="invalid-feedback">Heading is not valid</div>
-    </div>
-    <div class="form-group">
-        <input class="form-control py-4" type="text" placeholder="Post subheading" />
-        <div class="invalid-feedback">Subheading required</div>
-        <div class="invalid-feedback">Subheading is not valid</div>
-    </div>
-    <div class="form-group">
-        <input class="form-control py-4" type="text" placeholder="Post background image (enter a URL)" />
-        <div class="form-text text-white-50">Use an image URL 
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-8">
+                <h5 class="text-white text-center mt-5 mb-4">Create a New Wiki</h5>
+                <form>
+                    <div class="form-group mb-3">
+                        <input class="form-control py-4 w-100" type="text" placeholder="Wiki Title" required/>
+                    </div>
+                    <div class="form-group mb-3">
+                        <textarea class="form-control py-4 w-100" rows="8" placeholder="Full Article Content" required></textarea>
+                    </div>
+                    <div class="form-group mb-3">
+                        <label class="text-white-50">Category:</label>
+                        <select class="form-control py-2 w-100" required>
+                            <option value="">Select a Category</option>
+                            <option value="history">History</option>
+                            <option value="science">Science</option>
+                            <option value="technology">Technology</option>
+                        </select>
+                    </div>
+                    <div class="form-group mb-4">
+                        <label for="imageUpload" class="text-white-50">Upload Images:</label>
+                        <input type="file" class="form-control-file">
+                    </div>
+                    <div class="form-group mb-4">
+                        <button class="btn btn-primary w-100" type="submit">Create Wiki</button>
+                    </div>
+                </form>
+            </div>
         </div>
-        <div class="invalid-feedback">Background image required</div>
-        <div class="invalid-feedback">Background image is not valid</div>
     </div>
-    <div class="form-group">
-        <textarea class="form-control py-3"></textarea>
-        <div class="form-text text-white-50"><fa-icon class="mr-1"></fa-icon>Markdown
-            supported</div>
-        <div class="invalid-feedback">Body content required</div>
-        <div class="invalid-feedback">Body content is not valid</div>
-    </div>
-          <div class="form-group d-flex align-items-center justify-content-between">
-            <button class="btn btn-primary" type="submit">Create Post</button>
-          </div>
-        </form>
-      </div>
-    </div>
-  </div>
 </section>
