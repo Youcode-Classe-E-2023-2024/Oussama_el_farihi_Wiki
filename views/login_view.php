7653cdcaa2313fb6755d8b9c6f31ffe3b7b06f39
oussamaef06 <oussamaelfarihi@gmail.com>
2024-01-08 18:48:49 +0100

    updating login front

---
+++ views/login_view.php
@@ -0,0 +1,21 @@
+<section class="min-vh-100 d-flex justify-content-center align-items-center bg-dark">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-5">
+                <h5 class="text-white text-center mt-5 mb-4">Enter your informations</h5>
+                <form>
+                    <div class="form-group">
+                        <input class="form-control py-4 w-100 mb-2" type="text" placeholder="email"/>
+                        <input class="form-control py-4 w-100 mb-3" type="password" placeholder="Password"/>
+                        <div class="text-center mb-4">
+                        <span class="text-white-50">Don't have an account?</span>
+                        <a href="index.php?page=register" class="text-primary font-weight-bold">Register here <i class="fas fa-angle-double-right"></i></a>
+                    </div>
+                    <div class="form-group">
+                        <button class="btn btn-primary w-100" type="submit">Login</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
