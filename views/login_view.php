dd757884a55d3714525284448af805d01684fdd2
oussamaef06 <oussamaelfarihi@gmail.com>
2024-01-09 15:54:19 +0100

    add names and fix  forms

--- views/login_view.php
+++ views/login_view.php
@@ -3,12 +3,12 @@
         <div class="row justify-content-center">
             <div class="col-lg-5">
                 <h5 class="text-white text-center mt-5 mb-4">Enter your informations</h5>
-                <form>
+                <form method="POST" action="index.php?page=login">
                     <div class="form-group">
-                        <input class="form-control py-4 w-100 mb-2" type="text" placeholder="email"/>
-                        <input class="form-control py-4 w-100 mb-3" type="password" placeholder="Password"/>
+                        <input class="form-control py-4 w-100 mb-2" name="email" type="text" placeholder="email"/>
+                        <input class="form-control py-4 w-100 mb-3" name="password" type="password" placeholder="Password"/>
                     <div class="form-group">
-                        <button class="btn btn-primary w-100" type="submit">Login</button>
+                        <button class="btn btn-primary w-100" name="login" type="submit">Login</button>
                     </div>
                     <div class="text-center mb-4">
                         <span class="text-white-50">Don't have an account?</span>
--- views/register_view.php
+++ views/register_view.php
@@ -3,18 +3,18 @@
         <div class="row justify-content-center">
             <div class="col-lg-6">
                 <h5 class="text-white text-center mt-5 mb-4">Create Your Account</h5>
-                <form>
+                <form method="post" action="index.php?page=register">
                     <div class="form-group mb-3">
-                        <input class="form-control py-4 w-100" type="text" placeholder="Full Name" required/>
+                        <input class="form-control py-4 w-100" name="name" type="text" placeholder="Full Name" required/>
                     </div>
                     <div class="form-group mb-3">
-                        <input class="form-control py-4 w-100" type="email" placeholder="Email Address" required/>
+                        <input class="form-control py-4 w-100" name="email" type="email" placeholder="Email Address" required/>
                     </div>
                     <div class="form-group mb-3">
-                        <input class="form-control py-4 w-100" type="password" placeholder="Password" required/>
+                        <input class="form-control py-4 w-100" name="password" type="password" placeholder="Password" required/>
                     </div>
                     <div class="form-group mb-4">
-                        <button class="btn btn-primary w-100" type="submit">Register</button>
+                        <button class="btn btn-primary w-100" name="submit" type="submit">Register</button>
                     </div>
                     <div class="text-center">
                         <span class="text-white-50">Already have an account?</span>
