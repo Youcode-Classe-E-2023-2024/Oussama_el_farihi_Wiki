666f47d894942c78d275f713ab70528b6f8efa92
oussamaef06 <oussamaelfarihi@gmail.com>
2024-01-10 13:05:08 +0100

    fix argument passed to constructor

--- controllers/login_controller.php
+++ controllers/login_controller.php
@@ -1,6 +1,6 @@
 <?php
     if(isset($_POST['login'])){
-        $newUser = new User($_POST["email"], $_POST["password"]);
+        $newUser = new User($_POST["email"], null, $_POST["password"]);
         $newUser->login();
     } 
 ?>
