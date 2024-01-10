ac8a03f6d710daac38f3004f987cc5b326ad128e
oussamaef06 <oussamaelfarihi@gmail.com>
2024-01-10 10:27:58 +0100

    change from mysqli to pdo

--- _config/db.php
+++ _config/db.php
@@ -1,3 +1,12 @@
 <?php
 
-$db = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+try {
+    $db = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
+
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // echo "Connected successfully";
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+
+?>
