19c7206449a6e173857cf809b5a8dea1cfabe8bb
oussamaef06 <oussamaelfarihi@gmail.com>
2024-01-11 15:39:08 +0100

    creating categories class

---
+++ _classes/Categories.php
@@ -0,0 +1,50 @@
+<?php
+
+class Categories {
+    public $id;
+    public $name;
+    public $description;
+
+    public function __construct($name, $description)
+    {
+        $this->name = $name;
+        $this->description = $description;
+    }
+
+    public function createCategory($name, $description)
+    {
+        global $db;
+
+        $stmt = $db->prepare("INSERT INTO categories (name, description) VALUES (:name, :description)");
+        $stmt->bindParam(':name', $name, PDO::PARAM_STR);
+        $stmt->bindParam(':description', $description, PDO::PARAM_STR);
+
+        return $stmt->execute();
+    }
+
+    public function updateCategory($id, $name, $description)
+    {
+        global $db;
+
+        $stmt = $db->prepare("UPDATE categories SET name = :name, description = :description WHERE id = :id");
+        $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+        $stmt->bindParam(':name', $name, PDO::PARAM_STR);
+        $stmt->bindParam(':description', $description, PDO::PARAM_STR);
+
+        return $stmt->execute();
+    }
+
+    public function deleteCategory($id)
+    {
+        global $db;
+
+        $stmt = $db->prepare("DELETE FROM categories WHERE id = :id");
+        $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+
+        return $stmt->execute();
+    }
+
+    // Additional methods can be added here, such as getCategoryById(), getAllCategories(), etc.
+}
+
+?>
