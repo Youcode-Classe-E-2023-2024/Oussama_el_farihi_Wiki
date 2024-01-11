5488102f60d7c1d991c3edb9f97ee0036500a033
oussamaef06 <oussamaelfarihi@gmail.com>
2024-01-11 11:32:21 +0100

    /

--- views/_layout.php
+++ views/_layout.php
@@ -22,6 +22,8 @@
 
     <link href="assets/css/style.css" rel="stylesheet" />
 
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 </head>
 
