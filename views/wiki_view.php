8a38a283e80e6d9357e3b1c329aac0ad95123389
oussamaef06 <oussamaelfarihi@gmail.com>
2024-01-12 13:56:44 +0100

    wiki details view

---
+++ views/wiki_view.php
@@ -0,0 +1,18 @@
+<header class="masthead">
+    <div class="overlay"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-md-10 mx-auto">
+                <div class="site-heading">
+                        <h1></h1>
+                        <span class="subheading"></span>
+                    </div>
+            </div>
+        </div>
+    </div>
+</header>
+<div class="post-heading">
+        <h1></h1>
+        <h2 class="subheading"></h2>
+        <span class="meta"></span>
+    </div>
