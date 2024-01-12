8cf720c4f06ea9fe08a1a328886faf26a683df16
oussamaef06 <oussamaelfarihi@gmail.com>
2024-01-12 15:21:25 +0100

    adding wiki view and logic

--- views/wiki_view.php
+++ views/wiki_view.php
@@ -4,15 +4,25 @@
         <div class="row">
             <div class="col-lg-8 col-md-10 mx-auto">
                 <div class="site-heading">
-                        <h1></h1>
-                        <span class="subheading"></span>
-                    </div>
+                    <h1><?= htmlspecialchars($wiki['title']) ?></h1>
+                </div>
             </div>
         </div>
     </div>
 </header>
-<div class="post-heading">
-        <h1></h1>
-        <h2 class="subheading"></h2>
-        <span class="meta"></span>
+<!-- Post Content-->
+<article class="mb-4">
+    <div class="container px-4 px-lg-5">
+        <div class="row gx-4 gx-lg-5 justify-content-center">
+            <div class="col-md-10 col-lg-8 col-xl-7">
+                <p><?= nl2br(htmlspecialchars($wiki['content'])) ?></p>
+                <!-- Optionally, add author and date -->
+                <p class="post-meta">
+                    Posted by
+                    <a href="#!"><?= htmlspecialchars($wiki['name']) ?></a>
+                    on <?= date('F j, Y', strtotime($wiki['date_created'])) ?>
+                </p>
+            </div>
+        </div>
     </div>
+</article>
