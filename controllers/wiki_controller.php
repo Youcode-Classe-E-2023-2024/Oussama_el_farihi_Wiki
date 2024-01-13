01b4061efac9265e3ede8f0df784f7eb52e481d3
oussamaef06 <oussamaelfarihi@gmail.com>
2024-01-13 02:05:19 +0100

    displaty wiki details with all informations

--- controllers/wiki_controller.php
+++ controllers/wiki_controller.php
@@ -1,4 +1,16 @@
 <?php
 
 $wikiClass = new Wiki(null, null, null, null, null);
-$wikis = $wikiClass->getAllWikis();
+
+if (isset($_GET['id']) && is_numeric($_GET['id'])) {
+    $wikiId = $_GET['id'];
+    $wiki = $wikiClass->getDetailedWikiById($wikiId);
+
+// echo "<pre>";
+// print_r($wiki);
+// echo "<pre>";
+
+}
+
+
+?>
--- views/wiki_view.php
+++ views/wiki_view.php
@@ -1,4 +1,4 @@
-<header class="masthead">
+<header class="masthead" style="background-image: url('assets/img/<?= $wiki['img'] ?>') ">
     <div class="overlay"></div>
     <div class="container">
         <div class="row">
@@ -10,18 +10,23 @@
         </div>
     </div>
 </header>
-<!-- Post Content-->
+
 <article class="mb-4">
     <div class="container px-4 px-lg-5">
         <div class="row gx-4 gx-lg-5 justify-content-center">
             <div class="col-md-10 col-lg-8 col-xl-7">
                 <p><?= nl2br(htmlspecialchars($wiki['content'])) ?></p>
-                <!-- Optionally, add author and date -->
                 <p class="post-meta">
-                    Posted by
-                    <a href="#!"><?= htmlspecialchars($wiki['name']) ?></a>
+                    Posted by <?= htmlspecialchars($wiki['author_name']) ?>
                     on <?= date('F j, Y', strtotime($wiki['date_created'])) ?>
                 </p>
+                <p>Tags: 
+        <?php
+            $tags = explode(', ', $wiki['tags']);
+            foreach ($tags as $tag): ?>
+                <span class="badge badge-secondary"><?= htmlspecialchars($tag) ?></span>
+        <?php endforeach; ?>
+    </p>
             </div>
         </div>
     </div>
